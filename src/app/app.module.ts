9fe0f5aead4bbfd8578193a723707c20a65945ad
Rich Eodice <rich@eodice.com>
2020-09-03 10:58:05 -0400

    framing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { PostsComponent } from './posts/posts.component';
 import { EventsComponent } from './events/events.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { TreeComponent } from './tree/tree.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { TreeComponent } from './tree/tree.component';
     PostsComponent,
     EventsComponent,
     GalleryComponent,
-    TreeComponent
+    TreeComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
--- src/app/sidebar/sidebar.component.spec.ts
+++ src/app/sidebar/sidebar.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TreeComponent } from './tree.component';
+import { SidebarComponent } from './sidebar.component';
 
-describe('TreeComponent', () => {
-  let component: TreeComponent;
-  let fixture: ComponentFixture<TreeComponent>;
+describe('SidebarComponent', () => {
+  let component: SidebarComponent;
+  let fixture: ComponentFixture<SidebarComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TreeComponent ]
+      declarations: [ SidebarComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TreeComponent);
+    fixture = TestBed.createComponent(SidebarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
